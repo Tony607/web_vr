18e6888d92525e23753c8ed5fab5268a5a099fcc
Tony607 <zcw607@gmail.com>
2014-09-14 18:43:00 -0400

    add orientation control

--- server/websocket.js
+++ server/websocket.js
@@ -1,3 +1,10 @@
+/**define constant related to the servos*/
+//the 90 degree servo unit
+var servoOffsets = [0x59, 0x49, 0x45];
+//the clamp unit for each servo
+var servoLimits = [[0x18,0xA7],[0x02, 0x95],[0x06,0x90]];
+var degreePerUnit = 180.0/143.0;
+
 var clientdeviceorientation = {
 	alpha: 0,
 	beta: 0,
@@ -11,7 +18,15 @@ function crateSerialPortData(orientation) {
 	return str;
 }
 var arduinoPort;
-var serialportName = require("os").hostname()==="Arduino"?"ttyATH0":"ttyUSB0";
+/**
+in arduino yun the default serial port to 32u4 is named ttyATH0
+in Cubie Board 2 + arduino Pro Micro with usb connection,
+the serial port is named ttyACM0
+
+TODO: if needs support other ATmega328 based boards, the usb-serial connection 
+will be named ttyUSB0
+*/
+var serialportName = require("os").hostname()==="Arduino"?"ttyATH0":"ttyACM0";
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({port: 8080});
 wss.on('connection', function(ws) {
