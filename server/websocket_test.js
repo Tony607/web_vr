0941ce88a1c47a9c1170cd7ff363e6650c2eff35
chengwei.zhang <chengwei.zhang@intelligrated.com>
2014-10-08 17:50:47 -0400

    more stuff

---
+++ server/websocket_test.js
@@ -0,0 +1,23 @@
+var clientQuaternions;
+var WebSocketServer = require('ws').Server, wss = new WebSocketServer({
+		port : 8080
+	});
+wss.on('connection', function (ws) {
+	ws.on('message', function (message) {
+		try {
+			clientQuaternions = JSON.parse(message);
+		} catch (e) {
+			console.log(e);
+		}
+		if (clientQuaternions.length === 2) {
+			//set the two quaternion to the OrientationProcessor module
+			controls.setCameraWorldQuaternion(clientQuaternions[0]);
+			controls.setBodyWorldQuaternion(clientQuaternions[1]);
+			console.log("clientQuaternions",clientQuaternions);
+		} else {
+			console.log("Unknown format from Websocket. Expect two quaternions");
+		}
+	});
+	//TODO: might send something to notify the user that the robot is online and status
+	ws.send('something');
+});
