40c268651180ecfe1a62cb0539300795ac240fea
chengwei.zhang <chengwei.zhang@intelligrated.com>
2014-10-07 22:11:47 -0400

    more stuff

--- server/SurrogateServo/NodeSerial_TEST.js
+++ server/SurrogateServo/NodeSerial_TEST.js
@@ -1,5 +1,7 @@
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor
+var serialPacketBuffer = [0,0,0,0]; //length of 4
+var quaternion_raw_data = [0,0,0,0]; //this is the parsed data array
 
 var arduinoPort = new SerialPort("/dev/rfcomm1", {
 		baudrate : 115200,
@@ -7,7 +9,8 @@ var arduinoPort = new SerialPort("/dev/rfcomm1", {
 	});
 
 arduinoPort.on('data', function (data) {
-	console.log(data);
+	//console.log(data);
+	readQuaternionFromBuffer(data);
 });
 arduinoPort.on('close', function () {
 	console.log('arduinoPort->close');
@@ -15,3 +18,19 @@ arduinoPort.on('close', function () {
 arduinoPort.on('error', function () {
 	console.log('arduinoPort->error');
 });
+/**
+Function that read the incoming serial buffer and parse the quaternion data
+ */
+var readQuaternionFromBuffer = function (buf) {
+	for (var i; i < buf.length; i++) {
+		if (buf[i] === 0xFF) { //stop sign
+				//copy array
+				quaternion_raw_data = serialPacketBuffer.slice();
+				console.log("Quaternion:",quaternion_raw_data);
+		} else {
+			//FIFO
+			serialPacketBuffer.shift();
+			serialPacketBuffer[3] = buf[i];
+		}
+	}
+};
